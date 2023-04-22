59a2fc94b00c32530e9cc34d6cfd93ead59fb410
Rounak Datta <rounakdatta12@gmail.com>
2023-04-22 15:53:00 +0530

    Add functions to store embeddings to qdrant

--- ingest.py
+++ ingest.py
@@ -1,28 +1,51 @@
-"""Load html from files, clean up, split, ingest into Weaviate."""
-import pickle
-
-from langchain.document_loaders import ReadTheDocsLoader
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
+from langchain.docstore.document import Document
+
+from dataclasses import dataclass
+from datetime import datetime
+
+from vectordb import get_qdrant_impl
+
+@dataclass
+class MetaInformation:
+    source: str
+    timestamp: datetime
+    tags: List[str]
+
+@dataclass
+class Information:
+    meta: MetaInformation
+    info: str
 
 
-def ingest_docs():
-    """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
-    raw_documents = loader.load()
+def prepare_documents(information: Information) -> List[Document]:
+    metadata = {
+        "source": information.meta.source,
+        "timestamp": information.meta.timestamp,
+        "tags": information.meta.tags
+        }
+    return [Document(page_content=information.info, metadata=metadata)]
+
+
+def ingest_docs(information: Information):
+    # we structure our raw text as a Document, so that it becomes a standard datatype to work with
+    structured_documents = prepare_documents(information)
+
+    # we split the document cleverly
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
     )
-    documents = text_splitter.split_documents(raw_documents)
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(documents, embeddings)
+    chunked_documents = text_splitter.split_documents(structured_documents)
 
-    # Save vectorstore
-    with open("vectorstore.pkl", "wb") as f:
-        pickle.dump(vectorstore, f)
-
+    # we pass on the chunked documents to (1) be generated embeddings out of and (2) stored into qdrant
+    qdrant = get_qdrant_impl()
+    texts = [d.page_content for d in chunked_documents]
+    metadatas = [d.metadata for d in chunked_documents]
+    qdrant.add_texts(
+        texts=texts,
+        metadatas=metadatas
+    )
 
 if __name__ == "__main__":
     ingest_docs()
---
+++ vectordb.py
@@ -0,0 +1,19 @@
+import qdrant_client
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.qdrant import Qdrant
+
+client = qdrant_client.QdrantClient(
+    host="localhost",
+    port=6333,
+    prefer_grpc=False
+)
+
+embedding_generator = OpenAIEmbeddings()
+qdrant = Qdrant(
+    client=client,
+    collection_name="my_test_documents",
+    embedding_function=embedding_generator.embed_documents
+)
+
+def get_qdrant_impl():
+    return qdrant
